8837d2a8146a2cb5643913da0bc09a27256393ea
IfanWijaya <indodax@indodaxs-MacBook-Pro-2.local>
2020-03-17 19:12:12 +0700

    refactor, resolve build errors

--- pkg/lib/util/rpc.go
+++ pkg/lib/util/rpc.go
@@ -3,11 +3,15 @@ package util
 import(
     "bytes"
     "time"
+    "strings"
+    "strconv"
     "net/http"
+    "crypto/md5"
+    "crypto/sha256"
+    "encoding/hex"
 
     "github.com/divan/gorilla-xmlrpc/xml"
 
-    "github.com/btcid/wallet-services-backend/pkg/lib/auth"
     rc "github.com/btcid/wallet-services-backend/pkg/domain/rpcconfig"
 )
 
@@ -52,7 +56,7 @@ func (xr *XmlRpc) XmlRpcCall(method string, args *RpcReq, reply interface{}) err
 }
 
 func GenerateRpcReq(rpcConfig rc.RpcConfig, arg1 string, arg2 string, arg3 string) RpcReq {
-    hashkey, nonce := auth.GenerateHashkey(rpcConfig)
+    hashkey, nonce := generateHashkey(rpcConfig)
     
     return RpcReq{
         RpcUser : rpcConfig.User,
@@ -62,4 +66,23 @@ func GenerateRpcReq(rpcConfig rc.RpcConfig, arg1 string, arg2 string, arg3 strin
         Arg3    : arg3,
         Nonce   : nonce,
     }
+}
+
+func generateHashkey(rpcConfig rc.RpcConfig) (digestSha256String string, nonce string) {
+    mt    := Microtime()
+    nonce = strings.ReplaceAll(strconv.FormatFloat(mt, 'f', 9, 64), ".", "")
+
+    unixTime := time.Now().Unix()
+    this15m  := unixTime / 60
+
+    pass    := rpcConfig.Password
+    hashkey := rpcConfig.Hashkey
+
+    digest := pass + strconv.FormatInt(this15m, 10) + hashkey + nonce
+    digestMd5       := md5.Sum([]byte(digest))
+    digestMd5String := hex.EncodeToString(digestMd5[:])
+    digestSha256       := sha256.Sum256([]byte(digestMd5String))
+    digestSha256String = hex.EncodeToString(digestSha256[:])
+
+    return
 }
--- pkg/middlewares/logging.go
+++ pkg/middlewares/logging.go
@@ -1,7 +1,6 @@
 package middlewares
 
 import(
-    "fmt"
     "net/http"
 
     logger "github.com/btcid/wallet-services-backend/pkg/logging"
@@ -9,8 +8,8 @@ import(
 
 func LogMiddleware(hf http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        logger.InfoLog(r.URL + " hit. ", r)
+        logger.InfoLog(r.URL.String() + " hit. ", r)
         hf.ServeHTTP(w, r)
-        logger.InfoLog(r.URL + " done. ", r)
+        logger.InfoLog(r.URL.String() + " done. ", r)
     })
 }
