9d36e02887839ecd3f444b9c1fffcdebf3baf3e2
IfanWijaya <indodax@indodaxs-MacBook-Pro-2.local>
2020-04-17 14:24:55 +0700

    fix getblockcount btc

--- pkg/modules/btc/btcxmlrpc/getblockcount.go
+++ pkg/modules/btc/btcxmlrpc/getblockcount.go
@@ -8,15 +8,23 @@ import(
     "github.com/btcid/wallet-services-backend/pkg/lib/util"
 )
 
+type GetBlockCountNodeXmlRpcRes struct {
+    Blocks string
+}
+
 func (bs *BtcService) GetBlockCount(rpcConfig rc.RpcConfig) (*model.GetBlockCountRpcRes, error) {
     res := model.GetBlockCountRpcRes{ Blocks: "0" }
 
     rpcReq := util.GenerateRpcReq(rpcConfig, "", "", "")
     xmlrpc := util.NewXmlRpc(rpcConfig.Host, rpcConfig.Port, rpcConfig.Path)
 
-    err := xmlrpc.XmlRpcCall("getblockcount", &rpcReq, &res)
+    nodeRpcRes := GetBlockCountNodeXmlRpcRes{}
+
+    err := xmlrpc.XmlRpcCall("getblockcount", &rpcReq, &nodeRpcRes)
 
     if err == nil {
+        res.Blocks = nodeRpcRes.Blocks
+        
         return &res, nil
 
     } else if err != nil {
